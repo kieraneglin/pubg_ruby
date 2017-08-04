407312c217766d56d3a5ffe8cea246509503e5e4
Kieran Eglin <kieran.eglin@gmail.com>
2017-08-04 14:14:05 -0700

    added basic config tests

--- test/pubg_ruby_test.rb
+++ test/pubg_ruby_test.rb
@@ -6,11 +6,29 @@ class PubgRubyTest < Minitest::Test
     refute_nil ::PubgRuby::VERSION
   end
 
-  def test_it_does_something_useful
+  def test_that_api_key_is_settable
+    PubgRuby.configure do |config|
+      config.api_key = 'key'
+    end
+
+    assert_equal PubgRuby.configuration.api_key, 'key'
+  end
+
+  def test_that_setting_api_key_throws_nothing
     PubgRuby.configure do |config|
       config.api_key = ENV['API_KEY']
     end
 
-    ap PubgRuby::Profile.from_steam '76561198069157066'
+    PubgRuby::Profile.from_nickname ENV['TEST_NAME']
+  end
+
+  def test_that_not_setting_api_key_throws_something
+    PubgRuby.configure do |config|
+      config.api_key = nil
+    end
+
+    assert_raises ArgumentError do
+      PubgRuby::Profile.from_nickname ENV['TEST_NAME']
+    end
   end
 end
