d4eeed5dd033b0dcb813c39e0882e9f9ab030045
Kieran Eglin <kieran.eglin@gmail.com>
2017-08-04 14:20:20 -0700

    added a default API error catch

--- lib/pubg_ruby/errors.rb
+++ lib/pubg_ruby/errors.rb
@@ -1,2 +1,3 @@
 class ConfigurationError < StandardError; end
 class PlayerNotFoundError < StandardError; end
+class ApiError < StandardError; end
--- lib/pubg_ruby/profile.rb
+++ lib/pubg_ruby/profile.rb
@@ -4,14 +4,18 @@ module PubgRuby
   class Profile
     def self.from_nickname(nickname)
       stats = HTTP.headers('TRN-Api-Key' => PubgRuby.configuration.api_key)
-                  .get("https://pubgtracker.com/api/profile/pc/#{nickname}")
+        .get("https://pubgtracker.com/api/profile/pc/#{nickname}")
 
-      JSON.parse(stats)
+      begin
+        JSON.parse(stats)
+      rescue
+        raise ApiError, 'PUBGTracker API responded unexpectedly.  Please try again'
+      end
     end
 
     def self.from_steam(steam_id)
       stats = HTTP.headers('TRN-Api-Key' => PubgRuby.configuration.api_key)
-                  .get("https://pubgtracker.com/api/search?steamId=#{steam_id}")
+        .get("https://pubgtracker.com/api/search?steamId=#{steam_id}")
 
       begin
         from_nickname JSON.parse(stats)['Nickname']
